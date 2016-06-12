2655b26797d5b486cc67a91b77a2399a160e350d
Tao-xu <taoxu@163.com>
2016-06-12 18:46:58 +0800

    下载

--- applications/ball/admin.py
+++ applications/ball/admin.py
@@ -7,11 +7,11 @@ from applications.ball.models import Team, Game, UserGameShip, GameStat
 import datetime
 
 
-def delete_game_ship(modeladmin, request, queryset):
-    for item in queryset:
-        if not item.created_at.date() == datetime.date.today():
-            item.delete()
-delete_game_ship.short_description = u"清除用户下注记录"
+# def delete_game_ship(modeladmin, request, queryset):
+#     for item in queryset:
+#         if not item.created_at.date() == datetime.date.today():
+#             item.delete()
+# delete_game_ship.short_description = u"清除用户下注记录"
 
 
 def delete_user_game(modeladmin, request, queryset):
@@ -20,20 +20,20 @@ def delete_user_game(modeladmin, request, queryset):
         user.money += item.money
         user.save()
         item.delete()
-delete_user_game.short_description = u"清空用户下注记录"
+delete_user_game.short_description = u"清楚所选用户下注记录"
 
-
-def update_user_money(modeladmin, request, queryset):
-    user_ids = []
-    for item in queryset:
-        user = item.user
-        if user.id not in user_ids:
-            user.money = 0
-            user.save()
-        user.money -= item.money
-        user_ids.append(user.id)
-        user.save()
-update_user_money.short_description = u"更新用户余额"
+#
+# def update_user_money(modeladmin, request, queryset):
+#     user_ids = []
+#     for item in queryset:
+#         user = item.user
+#         if user.id not in user_ids:
+#             user.money = 0
+#             user.save()
+#         user.money -= item.money
+#         user_ids.append(user.id)
+#         user.save()
+# update_user_money.short_description = u"更新用户余额"
 
 
 class TeamAdmin(admin.ModelAdmin):
